7bfdcf676f7db457d5adb2d70c0890d9031b5820
Muhammad Rafi Isnaen <muhammadrafiisnaen@gmail.com>
2025-05-17 09:01:58 +0700

    Add typewriter
    Add typewriter component from aceternity in home page

--- sesi2/src/app/page.tsx
+++ sesi2/src/app/page.tsx
@@ -1,20 +1,27 @@
 import { FloatingNav } from "@/components/ui/floating-navbar";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect"; 
 
 export default function Home() {
-  const navItems = [
+  const navbarItems = [
     { name: "Home", link: "/", icon: null },
     { name: "About", link: "/about", icon: null },
     { name: "Contact", link: "/contact", icon: null },
     { name: "Experience", link: "/experience", icon: null },
     { name: "Resume", link: "/resume", icon: null },
   ];
+
   return (
     <>
-      <FloatingNav navItems={navItems} />
+      <FloatingNav navItems={navbarItems} />
       <div className="pt-28 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1 className="pt-25 text-4xl font-bold text-center">
-          Welcome to <span className="text-blue-500">Sesi 2</span>
+          Task : <span className="text-blue-500">Session 2</span>
         </h1>
+          <TypewriterEffect words={[
+            { text: "Welcome" },
+            { text: "to" },
+            { text: "My"},
+            { text :"Portofolio"}]} />
       </div>
     </>
   );
